715affded7ac0adaac0d1685906561fe4d6ace2c
Rurik <rurik.neologik@gmail.com>
2025-06-17 14:11:13 -0400

    pop
    pop

--- app/Models/SolicitudPopHeader.php
+++ app/Models/SolicitudPopHeader.php
@@ -6,5 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class SolicitudPopHeader extends Model
 {
-    //
+    protected $table = 'dbo.POP10200';
+    protected $primaryKey = 'POPRequisitionNumber';
+
+    protected $fillable = [
+        'POPRequisitionNumber',
+        'Requisition_Note_Index',
+        'RequisitionDescription',
+        'RequisitionStatus',
+        'COMMNTID',
+        'Comment_Note_Index',
+        'DOCDATE',
+        'REQDATE',
+        'REQSTDBY',
+        'PRSTADCD',
+        'CMPNYNAM',
+        'CONTACT',
+        'ADDRESS1',
+        'ADDRESS2',
+        'ADDRESS3',
+        'CITY',
+        'STATE',
+        'ZIPCODE',
+        'CCode',
+        'COUNTRY',
+        'PHONE1',
+        'PHONE2',
+        'PHONE3',
+        'FAX',
+        'DOCAMNT',
+        'CREATDDT',
+        'MODIFDT',
+        'USER2ENT',
+        'Flags',
+        'Workflow_Status',
+        'DomainUserName',
+        'USERDEF1',
+        'USERDEF2',
+        'DEX_ROW_TS',
+        'DEX_ROW_ID',
+    ];
 }
