cefc32e89abadc7a598af0d1a92de4c8a8b079ea
Rurik <rurik.neologik@gmail.com>
2025-06-23 12:35:29 -0400

    solicitud

--- app/Http/Controllers/OrdenCompraController.php
+++ app/Http/Controllers/OrdenCompraController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item_tax;
 use App\Models\solicitudes_compra;
 use App\Models\SolicitudPop;
 use App\Models\SolicitudPopHeader;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class OrdenCompraController extends Controller
 {
@@ -44,13 +47,13 @@ class OrdenCompraController extends Controller
                 $ITMNMBR = $item->ITEMNMBR;
                 $VENDORID = $item->VENDORID;
 
-                $DBObj->raw("
-                DECLARE @ITEMNMBR VARCHAR(30) = '$ITMNMBR', @VENDORID VARCHAR(15) = '$VENDORID'
-                SELECT COUNT(1) FROM IV00103 IV WHERE IV.ITEMNMBR = @ITEMNMBR AND IV.VENDORID = @VENDORID
-                IF (SELECT COUNT(1) FROM IV00103 IV WHERE IV.ITEMNMBR = @ITEMNMBR AND IV.VENDORID = @VENDORID) = 0
+                $itemTax = Item_tax::where('ITEMNMBR', $ITMNMBR)
+                    ->where('VENDORID', $VENDORID)
+                    ->count();
+                Log::info($ITMNMBR ." tiene tax : " . $itemTax);
 
-                BEGIN
-
+                if ($itemTax == 0) {
+                    $DBObj->statement("
                 INSERT INTO IV00103
                 (ITEMNMBR, VENDORID, ITMVNDTY, VNDITNUM, QTYRQSTN, QTYONORD, QTY_Drop_Shipped, LSTORDDT, LSORDQTY, LRCPTQTY, LSRCPTDT, LRCPTCST,
                 AVRGLDTM, NORCTITM, MINORQTY, MAXORDQTY, ECORDQTY, VNDITDSC, Last_Originating_Cost, Last_Currency_ID, FREEONBOARD, PRCHSUOM,
@@ -58,15 +61,37 @@ class OrdenCompraController extends Controller
                 SELECT IV.ITEMNMBR, PM.VENDORID, IV.ITEMTYPE, SUBSTRING(IV.ITEMDESC, 1, 30), 0, 0, 0, '01-01-01', 0, 0, '01-01-01', 0, 0, 0, 0, 0, 0, IV.ITEMDESC, IV.STNDCOST,
                 PM.CURNCYID, 1, '', 7 AS CURRNIDX, 0, 1, ''
                 FROM IV00101 IV, PM00200 PM
-                WHERE IV.ITEMNMBR = @ITEMNMBR AND PM.VENDORID = @VENDORID
-                END
-                IF (SELECT COUNT(1) FROM IV00101 IV, PM00200 PM WHERE IV.ITEMNMBR = @ITEMNMBR AND PM.VENDORID = @VENDORID) = 0
-                SELECT 1 as CodError, 'Id Articulo o Id Proveedor No Existe' as ErrorDesc
-                ELSE
-                SELECT 0 as CodError, 'Relacion Satisfactoria' as ErrorDesc
+                WHERE IV.ITEMNMBR = '$ITMNMBR' AND PM.VENDORID = '$VENDORID'
                 ");
+
+                }
+
+
+
+
+                $fecha = Carbon::now()->format('Y-m-d');
+                $PONUMBER = $item->POPRequisitionNumber;
+                $LOCNCODE =  $item->LOCNCODE;
+                $QUANTITY = $item->QTYORDER;
+                $QTYCANCELED = 0;//$item->QTYCANCELED;
+                $COMMENT = $item->Comment_Note_Index;
+                $UNITCOST = $item->UNITCOST;
+                $UOFM = $item->UOFM;
+                $ORD = $item->ORD;
+                $cod = '';
+                $msg = '';
+
+                $linea = $DBObj->select("EXEC taPoLine 1, ?, ?, ?, ?, default, ?, ?, '?', default, default, default, ?, default, default, default,
+default, default, default, default, default, default, default, default, ?, default, ?, default, default, default, default, default, default, default, default,
+default, default, default, default, ?, default, default, '', '', '', '', '', '', '', '', '', '', '', '', '', '', default, default, default, default, default, default,
+default, default, default, default, default, default, default, ? output, ? output",
+                    array($PONUMBER,$fecha,$VENDORID,$LOCNCODE,$ITMNMBR,$QUANTITY, $QTYCANCELED, $COMMENT,$UNITCOST,$UOFM,$ORD,$cod,$msg));
+                dump($linea);
+
+
             }
             $DBObj->commit();
+
         } catch (\Exception $e) {
             $DBObj->rollBack();
             throw $e; // O maneja el error de otra manera
